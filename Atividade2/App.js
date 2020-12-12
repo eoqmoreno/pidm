2273130bc9cdd1594eb05cdf1b8e195fc417a97c
George Moreno <gmodeveloper@gmail.com>
2020-12-12 00:57:50 -0300

    Atividade 2

---
+++ Atividade2/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import Home from './Atividade2/Home';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar backgroundColor={"#381859"}></StatusBar>
+
+      <Home></Home>
+
+    </View>
+  );
+}
