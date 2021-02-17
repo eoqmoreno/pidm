cd321d446dc90c651621ef1ba631ee7f9dd42512
George Moreno <gmodeveloper@gmail.com>
2021-02-16 23:13:28 -0300

    Atividade 5

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
-import IMCApp from './Atividade3/IMCApp';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RotasTelas from './Atividade5/src/componentes/Routes';
 
 export default function App() {
   return (
-    <View style={{flex:1}}>
-      <StatusBar backgroundColor={"#381859"}></StatusBar>
-      <IMCApp></IMCApp>
-    </View>
+    <SafeAreaProvider>
+      <RotasTelas></RotasTelas>
+    </SafeAreaProvider>
   );
   }
